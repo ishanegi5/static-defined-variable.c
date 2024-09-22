c75fc46cb262cb74923524808bb588c272350a2f
Isha Negi <itsishanegi@gmail.com>
2024-09-22 08:17:30 +0530

    Add files via upload

---
+++ static-defined-variable.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int fun()
+
+{ static int n=0;
+ 
+n++;
+
+return n;
+
+}
+
+int main()
+
+{ printf("%d ",fun());
+  
+printf("%d ",fun());
+  
+printf("%d \n",fun());
+
+return 0;
+
+}
